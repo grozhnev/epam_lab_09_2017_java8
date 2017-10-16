c3bc333758bcf9440d3815166a864188636e855b
Georgii Rozhnev <georgii_rozhnev@epam.com>
2017-10-17 00:04:40 +0300

    part1 exercise02 done.

--- lambda/src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
+++ lambda/src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
@@ -1,13 +1,17 @@
 package lambda.part1.exercise;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.FluentIterable;
+import com.google.common.base.Optional;
+
 import data.Person;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
 
 public class Lambdas02Exercise {
     @Test
@@ -19,7 +23,7 @@ public class Lambdas02Exercise {
         };
 
         // TODO use Arrays.sort
-
+        Arrays.sort(persons, (o1, o2) -> Integer.compare(o1.getAge(), o2.getAge()));
         assertArrayEquals(persons, new Person[]{
                 new Person("name 3", "lastName 3", 20),
                 new Person("name 2", "lastName 1", 30),
@@ -38,7 +42,13 @@ public class Lambdas02Exercise {
         Person person = null;
 
         // TODO use FluentIterable
-
-        assertEquals(person, new Person("name 1", "lastName 2", 30));
+        //assertEquals(person, new Person("name 1", "lastName 2", 30));
+        final Optional<Person> personOptional = FluentIterable.from(persons)
+                .firstMatch(p -> p != null && (p.getAge() == 30));
+        if (personOptional.isPresent()) {
+            personOptional.get().print();
+            assertNotNull(personOptional.get());
+            assertEquals(new Person("name 1", "lastName 2", 30), personOptional.get());
+        }
     }
 }
