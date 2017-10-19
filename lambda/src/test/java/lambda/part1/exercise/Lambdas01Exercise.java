5c09439be65a57edd25a6548b7bc7639d83d9d9b
Georgii Rozhnev <georgii_rozhnev@epam.com>
2017-10-19 20:00:55 +0300

    Split findFirstWithAge30() method from Lambdas01Excercise to Lambdas02Excercise.
    Lambdas02Exercise done.

--- lambda/src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
+++ lambda/src/test/java/lambda/part1/exercise/Lambdas01Exercise.java
@@ -1,24 +1,26 @@
 package lambda.part1.exercise;
 
+import static com.google.common.collect.ImmutableList.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 
 import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
 
-import com.google.common.collect.ImmutableList;
 import data.Person;
 import org.junit.Test;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class Lambdas01Exercise {
 
     @Test
     public void sortPersonsByAge() {
-        Person[] persons = {
-                new Person("name 3", "lastName 3", 20),
+        Person[] persons = {new Person("name 3", "lastName 3", 20),
                 new Person("name 1", "lastName 2", 40),
                 new Person("name 2", "lastName 1", 30)
         };
@@ -29,14 +31,14 @@ public class Lambdas01Exercise {
         assertArrayEquals(persons, new Person[]{
                 new Person("name 3", "lastName 3", 20),
                 new Person("name 2", "lastName 1", 30),
-                new Person("name 1", "lastName 2", 40),
-        });
+                new Person("name 1", "lastName 2", 40),}
+        );
     }
 
 
     @Test
     public void findFirstWithAge30() {
-        List<Person> persons = ImmutableList.of(
+        List<Person> persons = of(
                 new Person("name 3", "lastName 3", 20),
                 new Person("name 1", "lastName 2", 30),
                 new Person("name 2", "lastName 1", 30)
@@ -45,13 +47,17 @@ public class Lambdas01Exercise {
         Person person = null;
 
         // TODO use FluentIterable
-        final Optional<Person> firstPersonWithAge30 = FluentIterable.from(persons).firstMatch(p -> p.getAge() == 30);
-        assertEquals(new Person("name 1", "lastName 2", 30),firstPersonWithAge30.get());
+        Optional<Person> firstPerson30yearsOld = FluentIterable.from(persons)
+                                                                .firstMatch(new Predicate<Person>() {
+                                                                    @Override
+                                                                    public boolean apply(Person personInput) {
+                                                                        return personInput.getAge() == 30;
+                                                                    }
+                                                                });
+        assertEquals(firstPerson30yearsOld.get(), new Person("name 1", "lastName 2", 30));
+
 
     }
-
-
-
 
 
 }
--- lambda/src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
+++ lambda/src/test/java/lambda/part1/exercise/Lambdas02Exercise.java
@@ -1,5 +1,8 @@
 package lambda.part1.exercise;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.FluentIterable;
+
 import com.google.common.collect.ImmutableList;
 import data.Person;
 import org.junit.Test;
@@ -7,6 +10,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -32,16 +36,13 @@ public class Lambdas02Exercise {
 
     @Test
     public void findFirstWithAge30() {
-        List<Person> persons = ImmutableList.of(
-                new Person("name 3", "lastName 3", 20),
-                new Person("name 1", "lastName 2", 30),
-                new Person("name 2", "lastName 1", 30)
-        );
+        List<Person> persons = ImmutableList.of(new Person("name 3", "lastName 3", 20), new Person("name 1", "lastName 2", 30), new Person("name 2", "lastName 1", 30));
 
         Person person = null;
 
         // TODO use FluentIterable
 
-        assertEquals(person, new Person("name 1", "lastName 2", 30));
+        final Optional<Person> firstPersonWithAge30 = FluentIterable.from(persons).firstMatch(p -> p.getAge() == 30);
+        assertEquals(new Person("name 1", "lastName 2", 30), firstPersonWithAge30.get());
     }
 }
